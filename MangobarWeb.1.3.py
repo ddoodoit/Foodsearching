296c92a1f79d494251bb9d3a3130da446131dd93
ddoodoit <psj7789@naver.com>
2025-08-26 13:21:14 +0900

    Update MangobarWeb.1.3.py

--- MangobarWeb.1.3.py
+++ MangobarWeb.1.3.py
@@ -336,11 +336,10 @@ def show_table_with_click(df):
 
     selected = grid_response.get('selected_rows', None)
     if selected is not None:
-        if selected is not None:
-            if (hasattr(selected, 'empty') and not selected.empty) or (not hasattr(selected, 'empty') and len(selected) > 0):
-                first_item = selected.iloc[0] if hasattr(selected, 'iloc') else selected[0]
+        if (hasattr(selected, 'empty') and not selected.empty) or (not hasattr(selected, 'empty') and len(selected) > 0):
+            first_item = selected.iloc[0] if hasattr(selected, 'iloc') else selected[0]
             lcns_no = first_item['인허가번호']
-            
+    
             # 사용자 라이선스 ID 세션에서 가져오기
             license_id = st.session_state.get("license_id", None)
             if license_id:
@@ -351,7 +350,7 @@ def show_table_with_click(df):
                     change_info = None
             else:
                 change_info = None
-
+    
             if change_info:
                 st.write("### 변경 정보")
                 df_change = pd.DataFrame(change_info)
@@ -468,6 +467,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
